4135fce492bbc6804ef0fb0ff77a478cec987185
p.torchilov <p.torchilov@gmail.com>
2014-11-07 08:23:07 +0300

    Add fourth lab.

---
+++ Lab04/Lab01/ApplicationForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab01
+{
+    partial class ApplicationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApplicationForm));
+            this.groupBoxScheme = new System.Windows.Forms.GroupBox();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.groupBoxTests = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBoxTests.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxScheme
+            // 
+            this.groupBoxScheme.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxScheme.BackgroundImage")));
+            this.groupBoxScheme.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBoxScheme.Location = new System.Drawing.Point(495, 13);
+            this.groupBoxScheme.Name = "groupBoxScheme";
+            this.groupBoxScheme.Size = new System.Drawing.Size(546, 384);
+            this.groupBoxScheme.TabIndex = 0;
+            this.groupBoxScheme.TabStop = false;
+            this.groupBoxScheme.Text = "Scheme";
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(932, 427);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(89, 31);
+            this.buttonTest.TabIndex = 6;
+            this.buttonTest.Text = "Start";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.ButtonTestClick);
+            // 
+            // groupBoxTests
+            // 
+            this.groupBoxTests.Controls.Add(this.listBox1);
+            this.groupBoxTests.Controls.Add(this.label1);
+            this.groupBoxTests.Location = new System.Drawing.Point(12, 13);
+            this.groupBoxTests.Name = "groupBoxTests";
+            this.groupBoxTests.Size = new System.Drawing.Size(474, 477);
+            this.groupBoxTests.TabIndex = 1;
+            this.groupBoxTests.TabStop = false;
+            this.groupBoxTests.Text = "Tests";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(14, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(454, 433);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X1 X2 X3 X4 X5 X6 X7        Cover";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Полином: F(x) = x7+x5+x3+x+1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(492, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Количество тактов LFSR: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(631, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // ApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1043, 502);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.groupBoxTests);
+            this.Controls.Add(this.groupBoxScheme);
+            this.Name = "ApplicationForm";
+            this.Text = "Лабораторная работа №4. Вариант 1.";
+            this.groupBoxTests.ResumeLayout(false);
+            this.groupBoxTests.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxScheme;
+        private System.Windows.Forms.GroupBox groupBoxTests;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+
+    }
+}
+
---
+++ Lab04/Lab01/ApplicationForm.cs
@@ -0,0 +1,285 @@
+﻿namespace Lab01
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Main class for start window.
+    /// </summary>
+    public partial class ApplicationForm : Form
+    {
+        #region Constants
+
+        /// <summary>
+        /// The combinations count
+        /// </summary>
+        private readonly int combinationsCount = (int)Math.Pow(2, 7);
+
+        /// <summary>
+        /// The inputs number
+        /// </summary>
+        private const int InputsNumber = 7;
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// The cover
+        /// </summary>
+        private readonly bool[] cover = new bool[13 * 2]; 
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationForm"/> class.
+        /// </summary>
+        public ApplicationForm()
+        {
+            InitializeComponent();
+        } 
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Buttons the test click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void ButtonTestClick(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            var inputs = GetInputs(new BitArray(new[] { 1 }) { Length = InputsNumber });
+
+            var count = combinationsCount;
+            var startCase = new BitArray(new[] { 1 }) { Length = InputsNumber };
+
+            foreach (var input in inputs)
+            {
+                var inputCases = GetInputs(input);
+
+                for (var i = 0; i < inputCases.Count; i++)
+                {
+                    for (byte faultNumber = 0; faultNumber < 13; faultNumber++)
+                    {
+                        for (byte faultType = 0; faultType <= 1; faultType++)
+                        {
+                            var correctFunc = CalculateFunction(inputCases[i]);
+                            var realFunc = this.CalculateFunction(inputCases[i], faultNumber + 1, faultType != 0);
+
+                            if (correctFunc == realFunc)
+                            {
+                                continue;
+                            }
+
+                            cover[faultNumber * 2 + faultType] = true;
+                        }
+                    }
+
+                    if (IsCovereged() && i < count)
+                    {
+                        count = i;
+                        startCase = input;
+                        ResetCover();
+                        break;
+                    }
+                }
+
+                ResetCover();
+            }
+
+            var resultInputs = this.GetInputs(startCase);
+
+            for (byte i = 0; i <= count; i++)
+            {
+                for (byte faultNumber = 0; faultNumber < 13; faultNumber++)
+                {
+                    for (byte faultType = 0; faultType <= 1; faultType++)
+                    {
+                        var correctFunc = CalculateFunction(resultInputs[i]);
+                        var realFunc = this.CalculateFunction(resultInputs[i], faultNumber + 1, faultType != 0);
+
+                        if (correctFunc == realFunc)
+                        {
+                            continue;
+                        }
+
+                        cover[faultNumber * 2 + faultType] = true;
+
+                    }
+                }
+
+                var bits = BitArrayToString(resultInputs[i]);
+
+                listBox1.Items.Add(AddCover(bits));
+            }
+
+            label4.Text = count.ToString(CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Adds the cover.
+        /// </summary>
+        /// <param name="inputs">The inputs.</param>
+        /// <returns></returns>
+        private String AddCover(String inputs)
+        {
+            var builder = new StringBuilder(inputs);
+
+            builder.Append("   ");
+
+            for (byte i = 0; i < cover.Length; i++)
+            {
+                builder.Append(cover[i] == false ? "0 " : "1 ");
+
+                if (i % 2 == 1)
+                {
+                    builder.Append("  ");
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Determines whether this instance is covereged.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsCovereged()
+        {
+            for (byte i = 0; i < cover.Length; i++)
+            {
+                if (cover[i] == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Resets the cover.
+        /// </summary>
+        private void ResetCover()
+        {
+            for (byte i = 0; i < cover.Length; i++)
+            {
+                cover[i] = false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the inputs.
+        /// </summary>
+        /// <param name="startInput">The start input.</param>
+        /// <returns></returns>
+        private List<BitArray> GetInputs(BitArray startInput)
+        {
+            var lfsr = new LfsrUtil();
+
+            lfsr.Generate(startInput);
+
+            var inputs = lfsr.GetAllCombinations;
+
+            return inputs;
+        }
+
+        /// <summary>
+        /// Calculates the function.
+        /// </summary>
+        /// <param name="inputs">The inputs.</param>
+        /// <param name="faultNumber">The fault number.</param>
+        /// <param name="faultType">if set to <c>true</c> [fault type].</param>
+        /// <returns></returns>
+        private bool CalculateFunction(BitArray inputs, int faultNumber = -1, bool faultType = false)
+        {
+            var saveInput = false;
+
+            if (faultNumber >= 7 && faultNumber <= 13)
+            {
+                saveInput = inputs[faultNumber - 7];
+                inputs[faultNumber - 7] = faultType;
+            }
+
+            var f1 = !(inputs[0] | inputs[1]);
+            var f2 = !inputs[2];
+            var f3 = !(inputs[4] & inputs[5]);
+
+            bool f4, f5, f6;
+
+            if (faultNumber == 3)
+            {
+                f4 = !(inputs[3] & faultType & inputs[6]);
+            }
+            else
+            {
+                f4 = !(inputs[3] & f3 & inputs[6]);
+            }
+
+            if (faultNumber == 2)
+            {
+                f5 = faultType | f4;
+            }
+            else if (faultNumber == 4)
+            {
+                f5 = f2 | faultType;
+            }
+            else
+            {
+                f5 = f2 | f4;
+            }
+
+            if (faultNumber == 5)
+            {
+                f6 = f1 & faultType;
+            }
+            else if (faultNumber == 1)
+            {
+                f6 = faultType & f5;
+            }
+            else if (faultNumber == 6)
+            {
+                f6 = faultType;
+            }
+            else
+            {
+                f6 = f1 & f5;
+            }
+
+            if (faultNumber >= 7 && faultNumber <= 13)
+            {
+                inputs[faultNumber - 7] = saveInput;
+            }
+
+            return f6;
+        }
+
+        /// <summary>
+        /// Bits the array automatic string.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns></returns>
+        private string BitArrayToString(BitArray array)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < array.Length; i++)
+            {
+                sb.Append(array[i] ? "1   " : "0   ");
+            }
+
+            return sb.ToString();
+        } 
+
+        #endregion
+    }
+}
