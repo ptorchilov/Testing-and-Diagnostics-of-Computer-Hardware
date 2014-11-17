ea5901e0f8017b6801d627fb54004353494b5626
Pavel Torchilov <p.torchilov@itransition.com>
2014-11-17 16:44:12 +0300

    Initial commit. Add matrix multiplication functionality.

---
+++ Lab06/Lab06/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var matrix = new Matrix(8, 8);
+            var firstRow = new int[8] {0, 1, 0, 0, 1, 0, 0, 1};
+
+            matrix = Matrix.SetFirstRow(firstRow, matrix);
+
+            Matrix.ShowMatrix(matrix);
+
+            matrix = Matrix.SetRunningOne(matrix);
+
+            Matrix.ShowMatrix(matrix);
+
+            matrix = Matrix.XorMutliplication(matrix, matrix);
+
+            Matrix.ShowMatrix(matrix);
+        }
+
+
+    }
+}
---
+++ Lab06/Lab06/Matrix.cs
@@ -0,0 +1,105 @@
+﻿namespace Lab06
+{
+    using System;
+    using System.Text;
+    using System.Windows.Forms;
+
+    public class Matrix
+    {
+        private readonly int[,] matrix;
+
+        public Matrix(int dim1, int dim2)
+        {
+            matrix = new int[dim1, dim2];
+        }
+
+        public int Height
+        {
+            get { return matrix.GetLength(0); }
+        }
+
+        public int Width
+        {
+            get { return matrix.GetLength(1); }
+
+        }
+
+        public int this[int x, int y]
+        {
+            get { return matrix[x, y];  }
+            set { matrix[x, y] = value; }
+        }
+
+        public static Matrix XorMutliplication(Matrix matrix1, Matrix matrix2)
+        {
+            var resultMatrix = new Matrix(matrix1.Height, matrix2.Width);
+
+            for (var i = 0; i < resultMatrix.Height; i++)
+            {
+                for (var j = 0; j < resultMatrix.Width; j++)
+                {
+                    resultMatrix[i, j] = 0;
+
+                    for (var k = 0; k < matrix1.Width; k++)
+                    {
+                        resultMatrix[i, j] ^= matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        public static Matrix SetRunningOne(Matrix matrix)
+        {
+            for (var i = 1; i < matrix.Height; i++)
+            {
+                for (var j = 0; j < matrix.Width; j++)
+                {
+                    if (j == i - 1)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        public static Matrix SetFirstRow(int[] firstRow, Matrix matrix)
+        {
+            if (firstRow.Length != matrix.Width)
+            {
+                throw new ArgumentException("Length if row on equal matrix row length.");
+            }
+
+            for (var j = 0; j < matrix.Width; j++)
+            {
+                matrix[0, j] = firstRow[j];
+            }
+
+            return matrix;
+        }
+
+        public static void ShowMatrix(Matrix matrix)
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < matrix.Width; i++)
+            {
+                for (var j = 0; j < matrix.Height; j++)
+                {
+                    sb.Append(matrix[i, j]).Append(" ");
+                }
+
+                sb.Append(Environment.NewLine);
+            }
+
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
