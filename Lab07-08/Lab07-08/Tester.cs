7d56e39f4307225764b02c01b8f87b724dbc9202
Pavel Torchilov <p.torchilov@itransition.com>
2014-11-20 18:11:17 +0300

    Add class for Walking test.

---
+++ Lab07-08/Lab07-08/Tester.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab07_08
+{
+    using Tests;
+
+    public class Tester
+    {
+        // ReSharper disable once InconsistentNaming
+        public void RunWalking_0_1(Faults type, Memory memory)
+        {
+            switch (type)
+            {
+                case Faults.AF:
+                {
+                    Walking_0_1.TestAF(memory);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Lab07-08/Lab07-08/Tests/Walking_0_1.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab07_08.Tests
+{
+    // ReSharper disable once InconsistentNaming
+    public static class Walking_0_1
+    {
+        public static void TestAF(Memory memory)
+        {
+            for (var i = 0; i < memory.Width; i++)
+            {
+                for (var j = 0; j < memory.Height; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
